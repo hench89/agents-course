500a35bb9c355eaa68d3e540aa8f78bdd838f6c6
hench89 <henrik.c@maersk.com>
2025-05-27 10:39:43 +0200

    preselect questions for demo

--- src/agents_course/exam/run.py
+++ src/agents_course/exam/run.py
@@ -16,9 +16,9 @@ questions_to_solve = [
     #"840bfca7-4f7b-481a-8794-c560c340185d", # .. On June 6, 2023, an article by Carolyn Collins Petersen was published
 
     # -- solved --
-    #"8e867cd7-cff9-4e6c-867a-ff5ddc2550be", # .. How many studio albums were published by Mercedes Sosa between
+    "8e867cd7-cff9-4e6c-867a-ff5ddc2550be", # .. How many studio albums were published by Mercedes Sosa between
     #"2d83110e-a098-4ebb-9987-066c06fa42d0", # .. .rewsna eht sa ""tfel"" drow eht fo
-    #"cca530fc-4052-43b2-b130-b30968d8aa44", # .. Review the chess position provided in the image
+    "cca530fc-4052-43b2-b130-b30968d8aa44", # .. Review the chess position provided in the image
     #"4fc2f1ae-8625-45b5-ab34-ad4433bc21f8", # .. Who nominated the only Featured Article on English
     #"6f37996b-2ac7-44b0-8e68-6d28256631b4", # .. Given this table defining * on the set S = {a, b, c, d, e}
     #"3cef3a44-215e-4aed-8e3b-b1e3f08063b7", # .. I'm making a grocery list for my mom, but she's a
@@ -26,14 +26,11 @@ questions_to_solve = [
     #"305ac316-eef6-4446-960a-92d80d542f82", # .. Who did the actor who played Ray in the Polish-language version
     #"f918266a-b3e0-4914-865d-4faa564f1aef", # .. What is the final numeric output from the attached Python code
     #"3f57289b-8c60-48be-bd80-01f8099ca449", # .. How many at bats did the Yankee with the most walks
-    #"1f975693-876d-457b-a649-393859e79bf3", # .. Hi, I was out sick from my classes on Friday, so I'm trying to figure
+    "1f975693-876d-457b-a649-393859e79bf3", # .. Hi, I was out sick from my classes on Friday, so I'm trying to figure
     #"a0c07678-e491-4bbc-8f0b-07405144218f", # .. Who are the pitchers with the number before and after Taishō Tamai
     #"5a0c1adf-205e-4841-a666-7c3ef95def9d", # .. What is the first name of the only Malko Competition recipient from
     #"bda648d7-d618-4883-88f4-3466eabd860e", # .. Where were the Vietnamese specimens described by Kuznetzov in
     #"cf106601-ab4f-4af9-b045-5295fe67b37d", # .. What country had the least number of athletes at the 1928 Summer
-
-
-
 ]
 
 
